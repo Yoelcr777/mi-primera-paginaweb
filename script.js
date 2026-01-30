a1739b5adc155785b345b11b95acf2123123ec14
Yoelcr777 <elvisyoel137@gmail.com>
2026-01-29 19:17:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("miBoton").addEventListener("click", function () {
+  window.open(
+    "https://wa.me/51961408682?text=Hola%20quiero%20más%20información",
+    "_blank"
+  );
+});
